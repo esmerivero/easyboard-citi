683ffd732e2eb0effde6de6a22e35a6385513aa9
YocelinGRomero <garcia_romero.y@hotmail.com>
2018-08-11 16:18:24 -0500

    Drap y drop

--- src/js/main.js
+++ src/js/main.js
@@ -1,38 +1,43 @@
 const fill = document.querySelector('.fill');
 const empties = document.querySelectorAll('.empty');
 
-// Listeners para fill
+// Fill listeners
 fill.addEventListener('dragstart', dragStart);
 fill.addEventListener('dragend', dragEnd);
-// Loops
-for(const empty of empties) {
-  empty.addEventListener('dragover',dragOver);
-  empty.addEventListener('dragenter',dragEnter);
-  empty.addEventListener('dragleave',dragLeave);
-  empty.addEventListener('drop',dragDrop);
+
+// Loop through empty boxes and add listeners
+for (const empty of empties) {
+  empty.addEventListener('dragover', dragOver);
+  empty.addEventListener('dragenter', dragEnter);
+  empty.addEventListener('dragleave', dragLeave);
+  empty.addEventListener('drop', dragDrop);
 }
-// funciones
+
+// Drag Functions
+
 function dragStart() {
-  console.log('start');
-  this.className += 'hold';
+  this.className += ' hold';
   setTimeout(() => (this.className = 'invisible'), 0);
 }
 
 function dragEnd() {
-  console.log('end');
-  this.className = 'fill'; //comment
+  this.className = 'fill';
 }
 
 function dragOver(e) {
-console.log('over');
-e.preventDefault();
+  e.preventDefault();
 }
-function dragEnter() {
-console.log('enter');
+
+function dragEnter(e) {
+  e.preventDefault();
+  this.className += ' hovered';
 }
+
 function dragLeave() {
-console.log('leave');
+  this.className = 'empty';
 }
+
 function dragDrop() {
-console.log('drop');
+  this.className = 'empty';
+  this.append(fill);
 }
