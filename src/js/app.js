d438acba6a71a77dbef340e850d00a9fffc8b5d0
Montse2018 <anmmtz@outlook.com>
2018-08-11 16:36:42 -0500

    prueba

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,21 @@
 function creatTabla(){
-    let body = document.getElementsByTagName('body')[0]; //obtiene la referencia del elemento body
-    let tbl = document.createElement('table'); //crea el elemento table
-    let tblBody = document.createElement('tbody'); //crea el elemento tbody
+  miTabla = document.createElement("table");
+  miTabla.setAttribute("id", "miTabla");
+  miTabla.align = "center";
 
-    //creamos las celdas
-    for(let i=0; i<= 4; i++) {
-        //aqui se crean las hileras
-        let hileras = document.createElement('tr');
-        for(let j=0; j<4; j++) {
-            let celda = document.createElement('td');
-            let textoCelda = document.createTextNode('hilera es fila ' +i+', columna' +j);
-            celda.appendChild(textoCelda);
-            hilera.appendChild(celda);
-        }
-        tblBody.appendChild(hilera); //agrega la hilara al final del elemento
-    }
-    tabla.appendChild(tblBody);//posiciona el tbody debajo del elemento table
-    body.appendChild(tabla);
-    table.setAttribute('border', '2');
-}
+  f = 5;
+  c = 5;
+
+  for(let i=0; i<f; i++) {
+      tr = document.createElement("tr");
+
+      for(let j=0; j<c; j++) {
+          td = document.createElement("input");
+          td.setAttribute("type", "text");
+          td.setAttribute("placeholder", i+","+j);
+          tr.appendChild(td);
+      }
+      miTabla.appendChild(tr);
+  }
+
+  document.body.appendChild(miTabla);
